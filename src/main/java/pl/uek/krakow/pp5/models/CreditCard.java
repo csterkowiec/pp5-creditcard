eb6984b66a59ca8a14b6f08aae9209f0ff1e42ff
jacekku <kecajwy@gmail.com>
2019-10-24 21:39:07 +0200

    adding more tests

--- src/main/java/pl/uek/krakow/pp5/models/CreditCard.java
+++ src/main/java/pl/uek/krakow/pp5/models/CreditCard.java
@@ -1,15 +1,38 @@
 package pl.uek.krakow.pp5.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NonNull;
 
 import java.math.BigDecimal;
 
-@AllArgsConstructor
-@RequiredArgsConstructor
-@Data
+@Builder
+@Getter
 public class CreditCard {
+	@NonNull
 	private final String cardNumber;
+	@NonNull
 	private BigDecimal limit;
+	private BigDecimal money;
+
+	public void setLimit(BigDecimal amount) {
+		if (amount.compareTo(BigDecimal.valueOf(100)) < 0) {
+			throw new IllegalStateException("Cannot set limit below 100 PLN");
+		}
+		limit = amount;
+	}
+
+	public void setMoney(BigDecimal amount) {
+		if (amount.compareTo(BigDecimal.ZERO) < 0) {
+			throw new IllegalStateException("Cannot set money below zero");
+		}
+		money = money.subtract(amount);
+	}
+
+	public void withdraw(BigDecimal withdrawAmount) {
+		if (withdrawAmount.compareTo(limit) > 0) {
+			throw new IllegalStateException("Cannot withdraw above limit amount");
+		}
+		setMoney(money.subtract(withdrawAmount));
+	}
 }
--- src/test/java/pl/uek/krakow/pp5/models/CreditCardTest.java
+++ src/test/java/pl/uek/krakow/pp5/models/CreditCardTest.java
@@ -8,12 +8,49 @@ import java.math.BigDecimal;
 
 public class CreditCardTest {
 
-	private static final long LIMIT = 2000;
+	private static final BigDecimal LIMIT = BigDecimal.valueOf(2000);
+	private static final BigDecimal WITHDRAW_BELOW_LIMIT = BigDecimal.valueOf(1000);
+	private static final BigDecimal MONEY = BigDecimal.valueOf(1500);
+	private static final BigDecimal INVALID_LIMIT = BigDecimal.TEN;
+	private static final String CARD_NUMBER = "1234-1234-1234-1234";
 
 	@Test
-	public void shouldAllowLimitChange() {
-		CreditCard card = new CreditCard("1234-1234-1234-1234-1234");
-		card.setLimit(BigDecimal.valueOf(LIMIT));
-		Assert.assertEquals(card.getLimit(), BigDecimal.valueOf(LIMIT));
+	public void LimitChangeAboveLimitPasses() {
+		CreditCard card = CreditCard.builder()
+				.cardNumber(CARD_NUMBER)
+				.limit(LIMIT)
+				.build();
+		card.setLimit(WITHDRAW_BELOW_LIMIT);
+		Assert.assertEquals(card.getLimit(), WITHDRAW_BELOW_LIMIT);
+	}
+
+	@Test
+	public void WithdrawBelowLimitPasses() {
+		CreditCard card = CreditCard.builder()
+				.cardNumber(CARD_NUMBER)
+				.limit(LIMIT)
+				.money(MONEY)
+				.build();
+		card.withdraw(WITHDRAW_BELOW_LIMIT);
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void LimitChangeBelowLimitThrows() {
+		CreditCard.builder()
+				.cardNumber(CARD_NUMBER)
+				.limit(LIMIT)
+				.money(MONEY)
+				.build()
+				.setLimit(INVALID_LIMIT);
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void WithdrawBelowMoneyThrows() {
+		CreditCard.builder()
+				.cardNumber(CARD_NUMBER)
+				.limit(LIMIT)
+				.money(BigDecimal.ZERO)
+				.build()
+				.withdraw(BigDecimal.TEN);
 	}
 }
